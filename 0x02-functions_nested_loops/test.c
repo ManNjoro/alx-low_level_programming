6e9fb683fef303927f1c9feb077b2766feb86572
ManNjoro <elijohnmwoho@gmail.com>
2023-02-21 05:58:54 -0800

    Write-a-function-that-prints-the-9times-table,starting-with-0

--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -16,23 +16,28 @@ void times_table(void)
 		{
 			int product = i * j;
 
-			if (j == 0)
-			{
-				_putchar('0');
-			}
-
-			else if (product < 10)
+			if (j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+			}
+
+			if (product < 10)
+			{
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else if (product >= 10 && product < 100)
+			{
+				_putchar(' ');
+				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
 			}
 			else
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(product / 10 + '0');
+				_putchar(product / 100 + '0');
+				_putchar(product % 100 / 10 + '0');
 				_putchar(product % 10 + '0');
 			}
 			_putchar('\n');
