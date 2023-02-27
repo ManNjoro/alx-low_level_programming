e0c11dce6d00cf157fc1bc78df20d9bee3413d21
ManNjoro <elijohnmwoho@gmail.com>
2023-02-27 13:07:48 -0800

    function_that_prints_half_of_a_string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,26 +1,36 @@
 #include "main.h"
 
 /**
- * puts_half - prints half of a string
- * @str: string
- * Return: void
- */
-
+  * puts_half - Prints half of a string
+  * @str: string
+  *
+  * Return: void
+  */
 void puts_half(char *str)
 {
-	int i, j, max;
+	int j = 0;
+	int k;
 
-	i = 0;
-	while (str[i] != '\0')
+	while (str[j] != '\0')
 	{
-		i++;
-	}
-	max = i;
-	j = max / 2;
-	while (j <= max)
-	{
-		_putchar(str[j]);
 		j++;
 	}
+
+	if (j % 2 == 1)
+	{
+		k = (j - 1) / 2;
+		k += 1;
+	}
+	else
+	{
+		k = j / 2;
+	}
+
+	for (; k < j; k++)
+	{
+		_putchar(str[k]);
+	}
+
 	_putchar('\n');
 }
+
