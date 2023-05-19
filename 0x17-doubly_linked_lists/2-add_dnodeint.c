52790283c92ca0791165dbecac0077361f67903e
ManNjoro <elijohnmwoho@gmail.com>
2023-05-18 17:27:10 -0700

    corrections

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,19 +2,19 @@
 
 /**
  * add_dnodeint - inserts node at the beginning
- * @head - pointer to a pointer
- * @n - data to enter
+ * @head: pointer to a pointer
+ * @n: data to enter
  * Return: memory address of new node
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
+
 	new_node = malloc(sizeof(dlistint_t));
-	
 	new_node->n = n;
 	new_node->prev = NULL;
-	
+
 	if (new_node == NULL)
 		return (NULL);
 
