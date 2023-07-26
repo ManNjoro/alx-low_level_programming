87dc7472eb5571a94ef0409b8a1cf74e338557e5
ManNjoro <elijohnmwoho@gmail.com>
2023-07-26 15:19:08 -0700

    first commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 
+
 def island_perimeter(grid):
     """
-    Calculate the perimeter of the island described in the grid.
-
-    Args:
-    grid (list of list of integers): Represents the island grid.
-
-    Returns:
-    int: The perimeter of the island.
+    This function calculated the perimeter
+    of a grid
     """
-
-    perimeter = 0
-
-    # Check each cell in the grid
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:  # If it's land, calculate its perimeter
-                perimeter += 4  # Start with 4, as a single cell has 4 sides
-                # Check the adjacent cells (up, down, left, right)
-                if i > 0 and grid[i - 1][j] == 1:  # Up
-                    perimeter -= 2  # Reduce 2 sides (top and bottom)
-                if j > 0 and grid[i][j - 1] == 1:  # Left
-                    perimeter -= 2  # Reduce 2 sides (left and right)
-
-    return perimeter
+    pm = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if x == 0 or grid[x - 1][y] == 0:
+                    pm += 1
+                if y == 0 or grid[x][y - 1] == 0:
+                    pm += 1
+                if x == len(grid) - 1 or grid[x + 1][y] == 0:
+                    pm += 1
+                if y == len(grid[x]) - 1 or grid[x][y + 1] == 0:
+                    pm += 1
+    return (pm)
